aa36207b47dce4a0686d2f20c5c97480e341d915
Cloud11665 <Cloud11665@gmail.com>
2020-09-03 09:38:15 +0200

    big changes

--- main.py
+++ main.py
@@ -1,34 +1,23 @@
-import datetime
-
 import flask
 
 app = flask.Flask(__name__)
-app.secret_key = 'janpawel2jebalmaledzieci'
-app.permanent_session_lifetime = datetime.timedelta(weeks=1)
-
 
 @app.route('/')
 def home():
-	return flask.render_template('index.html')
+	return flask.render_template('home.html')
+
+@app.route('/projects')
+def projects():
+	return flask.render_template('projects.html')
+
+@app.route('/contact')
+def contact():
+	return flask.render_template('contact.html')
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-	if flask.request.method == 'POST':
-		flask.session.permanent = True
-		user = flask.request.form['nm']
-		flask.session['user'] = user
-		return flask.redirect(flask.url_for('user'))
-	else:
-		return flask.render_template('login.html')
-
-@app.route('/user')
-def user():
-	if 'user' in flask.session:
-		user = flask.session['user']
-		return f"<h1>{user}</h1>"
-	else:
-		return flask.redirect(flask.url_for('login'))
+	return flask.render_template('login.html')
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run()
