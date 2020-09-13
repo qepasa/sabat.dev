31f2ae44864e488edc54ba7b6d3d6ab4b96934bb
Cloud11665 <Cloud11665@gmail.com>
2020-09-13 16:39:13 +0200

    added meta tags

--- main.py
+++ main.py
@@ -2,22 +2,26 @@ import flask
 
 app = flask.Flask(__name__)
 
+
 @app.route('/')
 def home():
-	return flask.render_template('home.html')
+    return flask.render_template('home.html')
+
 
 @app.route('/projects')
 def projects():
-	return flask.render_template('projects.html')
+    return flask.render_template('projects.html')
+
 
 @app.route('/contact')
 def contact():
-	return flask.render_template('contact.html')
+    return flask.render_template('contact.html')
+
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-	return flask.render_template('login.html')
+    return flask.render_template('login.html')
 
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
+# coding=utf-8
 from main import app
 
 if __name__ == '__main__':
     app.run()
 
-#gunicorn --bind 127.0.0.1:5000 wsgi:app
+# gunicorn --bind 127.0.0.1:5000 wsgi:app
