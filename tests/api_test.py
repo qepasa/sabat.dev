7525966503ee7e2a6e8342a7abcaa35d7530a66a
Bartek Sabat <Cloud11665@gmail.com>
2020-12-13 23:08:50 +0100

    CI

---
+++ tests/api_test.py
@@ -0,0 +1,29 @@
+import pytest
+import requests
+
+
+def tta_test_basic():
+	resp = requests.get('localhost:5000/api/tta?c=1A')
+	assert resp['success']
+
+def tta_test_offset():
+	resp = requests.get('localhost:5000/api/tta?c=1A&o=-1')
+	assert resp['success']
+
+def sub_test_basic():
+	resp = requests.get('localhost:5000/api/sub?c=1A')
+	assert resp['success']
+
+def sub_test_offset():
+	resp = requests.get('localhost:5000/api/sub?c=1A&o=-1')
+	assert resp['success']
+
+def cla_test():
+	resp = requests.get('localhost:5000/api/cla')
+	assert resp['success']
+
+tta_test_basic()
+tta_test_offset()
+sub_test_basic()
+sub_test_offset()
+cla_test()
