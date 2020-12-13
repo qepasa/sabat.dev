7ad16d3f7152466ad6594df945c2cbcd819cac86
Bartek Sabat <Cloud11665@gmail.com>
2020-12-13 23:49:32 +0100

    fix api tests

--- tests/api_test.py
+++ tests/api_test.py
@@ -3,23 +3,23 @@ import requests
 
 
 def tta_test_basic():
-	resp = requests.get('localhost:5000/api/tta?c=1A')
+	resp = requests.get('localhost:5000/api/tta?c=1A').json()
 	assert resp['success']
 
 def tta_test_offset():
-	resp = requests.get('localhost:5000/api/tta?c=1A&o=-1')
+	resp = requests.get('localhost:5000/api/tta?c=1A&o=-1').json()
 	assert resp['success']
 
 def sub_test_basic():
-	resp = requests.get('localhost:5000/api/sub?c=1A')
+	resp = requests.get('localhost:5000/api/sub?c=1A').json()
 	assert resp['success']
 
 def sub_test_offset():
-	resp = requests.get('localhost:5000/api/sub?c=1A&o=-1')
+	resp = requests.get('localhost:5000/api/sub?c=1A&o=-1').json()
 	assert resp['success']
 
 def cla_test():
-	resp = requests.get('localhost:5000/api/cla')
+	resp = requests.get('localhost:5000/api/cla').json()
 	assert resp['success']
 
 tta_test_basic()
