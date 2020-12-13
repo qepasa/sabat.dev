e0418f1717fed162686baaa3fcf2cae345d1db09
Bartek Sabat <Cloud11665@gmail.com>
2020-12-13 23:53:08 +0100

    fix localhost url

--- tests/api_test.py
+++ tests/api_test.py
@@ -3,24 +3,24 @@ import requests
 
 
 def tta_test_basic():
-	resp = requests.get('localhost:5000/api/tta?c=1A').json()
-	assert resp['success']
+	resp = requests.get('http://localhost:5000/api/tta?c=1A')
+	assert resp.json()['success']
 
 def tta_test_offset():
-	resp = requests.get('localhost:5000/api/tta?c=1A&o=-1').json()
-	assert resp['success']
+	resp = requests.get('http://localhost:5000/api/tta?c=1A&o=-1')
+	assert resp.json()['success']
 
 def sub_test_basic():
-	resp = requests.get('localhost:5000/api/sub?c=1A').json()
-	assert resp['success']
+	resp = requests.get('http://localhost:5000/api/sub?c=1A')
+	assert resp.json()['success']
 
 def sub_test_offset():
-	resp = requests.get('localhost:5000/api/sub?c=1A&o=-1').json()
-	assert resp['success']
+	resp = requests.get('http://localhost:5000/api/sub?c=1A&o=-1')
+	assert resp.json()['success']
 
 def cla_test():
-	resp = requests.get('localhost:5000/api/cla').json()
-	assert resp['success']
+	resp = requests.get('http://localhost:5000/api/cla')
+	assert resp.json()['success']
 
 tta_test_basic()
 tta_test_offset()
