26e7eaa681db0b43ea650303e44b3127f0e8b0dd
Bartek Sabat <Cloud11665@gmail.com>
2020-12-14 00:13:12 +0100

    fix naming scheme

--- tests/api/sub_test_offset.py
+++ tests/api/sub_test_offset.py
@@ -1,5 +1,8 @@
 import pytest
 import requests
 
-resp = requests.get('http://localhost:5000/api/sub?c=1A&o=-1')
-assert resp.json()['success']
+def test():
+	resp = requests.get('http://localhost:5000/api/sub?c=1A&o=-1')
+	assert resp.json()['success']
+
+test()
